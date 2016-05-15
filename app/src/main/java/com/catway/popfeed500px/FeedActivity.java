7a463e34f2bd0b36b11585e8c6e8f8374929562f
ForestFox <yurii.kot@gmail.com>
2016-05-16 02:43:46 +0300

    Fix wrong symbol

--- app/src/main/java/com/catway/popfeed500px/FeedActivity.java
+++ app/src/main/java/com/catway/popfeed500px/FeedActivity.java
@@ -97,7 +97,7 @@ public class FeedActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {r
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.menu_refresh)
         {
